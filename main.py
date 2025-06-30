159c30fea2ed2d2eae91f68a0edb79b2c82079ac
dcjedlicka <dcjedlicka@gmail.com>
2025-06-29 21:04:09 -0500

    Working level 1 with winner

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
 import pymunk
+import typing as t
 import pygame
 import pymunk.pygame_util
+import random
 
-def addMarble(space : pymunk.Space, x : int, y : int):
+def addMarble(space : pymunk.Space, x : int, y : int) -> pymunk.Shape:
     body = pymunk.Body(1, pymunk.moment_for_circle(1, 0, 15))
     body.position = (x, y)
     shape = pymunk.Circle(body, 15)
     shape.elasticity = 0.9
+    #shape.color = pygame.Color(50, 200, 50, 1)
+    shape.color = (random.randint(10, 250), random.randint(10, 250), random.randint(10, 250), 1)  # RGB random color
     space.add(body, shape)
+    return shape
 
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((600, 600))
-    clock = pygame.time.Clock()
-    draw_options = pymunk.pygame_util.DrawOptions(screen)
-
-    space = pymunk.Space()
-    space.gravity = (0, 900)  # gravity pointing down
-
-    # Create a marble
-    addMarble(space, 102, 50)
-    addMarble(space, 202, 50)
-    addMarble(space, 302, 50)
-    addMarble(space, 402, 50)
-
+def create_level1(space: pymunk.Space) -> None:
     # Add a floor
     floor = pymunk.Segment(space.static_body, (0, 590), (600, 590), 5)
     floor.elasticity = 0.9
@@ -78,18 +68,58 @@ def main():
     space.add(flat1)
 
 
+def drawWinner(space : pymunk.Space, shape : pymunk.Shape) -> pymunk.Shape:
+    body = pymunk.Body(0, 0, pymunk.Body.STATIC)
+    body.position = (550, 50)
+    new_shape = pymunk.Circle(body, 15)
+    new_shape.elasticity = 0.9
+    new_shape.color = shape.color
+    space.add(body, new_shape)
+    return new_shape
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+    clock = pygame.time.Clock()
+    draw_options = pymunk.pygame_util.DrawOptions(screen)
+
+    space = pymunk.Space()
+    space.gravity = (0, 900)  # gravity pointing down
+
+    # Create marbles
+    marbles : t.List[pymunk.Shape] = []
+    marbles.append(addMarble(space, 102, 50))
+    marbles.append(addMarble(space, 202, 50))
+    marbles.append(addMarble(space, 302, 50))
+    marbles.append(addMarble(space, 402, 50))
+
+    # Create level
+    create_level1(space)
 
     running = True
+    winners : t.List[pymunk.Shape] = []
+    random_marble : pymunk.Shape = marbles[0]
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
         screen.fill((255, 255, 255))
         space.step(1/60)
         space.debug_draw(draw_options)
         pygame.display.flip()
         clock.tick(60)
+        random_marble.color = (random.randint(10, 250), random.randint(10, 250), random.randint(10, 250), 1)  # RGB random color
+        for m in marbles:
+            if m.body.position.x > 600:
+                space.remove(m)
+                marbles.remove(m)
+                winners.append(m)
+                if len(winners) == 1:
+                    new_marble = drawWinner(space, m)
+                    if random_marble == m:
+                        random_marble = new_marble
+
 
     pygame.quit()
 
